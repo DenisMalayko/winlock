6dc8d1eae98c4b7a768aff7ecd8663c8a2a534bf
Denchik Denchik <dnsmalayko@gmail.com>
2020-09-15 14:48:09 +0300

    home mobile

--- build/js/script.js
+++ build/js/script.js
@@ -100,8 +100,8 @@ $(document).ready(function(){
 					fade: true,
 					infinite: true,
 					arrows: true,
-					prevArrow: '.btn-prev',
-					nextArrow: '.btn-next',
+					prevArrow: '.folder-nav .btn-prev',
+					nextArrow: '.folder-nav .btn-next',
 					touchThreshold: 15
 				});
 
@@ -129,8 +129,285 @@ $(document).ready(function(){
 					let current = ZeroCount(currentSlide);
 					$('.folder-nav-count .current').text(current)
 				});
+
+				$('.doors-list a').on('click', function(){
+
+					$('.doors-list a').removeClass('active')
+					$(this).addClass('active')
+
+					$('.doors-photo').each(function(){
+						$(this).removeClass('active');
+					});
+					$($(this).attr('href')).addClass('active');
+					return false;
+
+				});
 			}
+
+			if($(window).width() < 768){
+				$('.warehouse-body').slick({
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					infinite: true,
+					arrows: true,
+					prevArrow: '.warehouse .btn-prev',
+					nextArrow: '.warehouse .btn-next',
+					touchThreshold: 15
+				})
+			}
+
+			$('.footer-item-top').on('click', function(){
+				$('.footer-item-list').not($(this).next()).removeClass('active');
+				$(this).next().toggleClass('active');
+			});
 
 		break;
 	}
 });
+
+
+
+
+// Init Map
+//----------------------------------------------------//
+var map, infoWindow, markersData = [{
+	lat: 48.323039,
+	lng: 25.917981
+}];
+
+function initMap() {
+	var e = {
+		center: new google.maps.LatLng(48.323039, 25.913981),
+		zoom: 15,
+		styles: [
+			{
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#ebe3cd"
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#523735"
+					}
+				]
+			},
+			{
+				"elementType": "labels.text.stroke",
+				"stylers": [
+					{
+						"color": "#f5f1e6"
+					}
+				]
+			},
+			{
+				"featureType": "administrative",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#c9b2a6"
+					}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#dcd2be"
+					}
+				]
+			},
+			{
+				"featureType": "administrative.land_parcel",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#ae9e90"
+					}
+				]
+			},
+			{
+				"featureType": "landscape.natural",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dfd2ae"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dfd2ae"
+					}
+				]
+			},
+			{
+				"featureType": "poi",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#93817c"
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "geometry.fill",
+				"stylers": [
+					{
+						"color": "#a5b076"
+					}
+				]
+			},
+			{
+				"featureType": "poi.park",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#447530"
+					}
+				]
+			},
+			{
+				"featureType": "road",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f5f1e6"
+					}
+				]
+			},
+			{
+				"featureType": "road.arterial",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#fdfcf8"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#f8c967"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#e9bc62"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway.controlled_access",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#e98d58"
+					}
+				]
+			},
+			{
+				"featureType": "road.highway.controlled_access",
+				"elementType": "geometry.stroke",
+				"stylers": [
+					{
+						"color": "#db8555"
+					}
+				]
+			},
+			{
+				"featureType": "road.local",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#806b63"
+					}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dfd2ae"
+					}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#8f7d77"
+					}
+				]
+			},
+			{
+				"featureType": "transit.line",
+				"elementType": "labels.text.stroke",
+				"stylers": [
+					{
+						"color": "#ebe3cd"
+					}
+				]
+			},
+			{
+				"featureType": "transit.station",
+				"elementType": "geometry",
+				"stylers": [
+					{
+						"color": "#dfd2ae"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "geometry.fill",
+				"stylers": [
+					{
+						"color": "#b9d3c2"
+					}
+				]
+			},
+			{
+				"featureType": "water",
+				"elementType": "labels.text.fill",
+				"stylers": [
+					{
+						"color": "#92998d"
+					}
+				]
+			}
+		]
+	};
+	map = new google.maps.Map(document.getElementById("map"), e), infoWindow = new google.maps.InfoWindow;
+	for (var a = 0; a < markersData.length; a++) {
+		addMarker(new google.maps.LatLng(markersData[a].lat, markersData[a].lng), markersData[a].name, markersData[a].address)
+	}
+}
+
+function addMarker(e, a, o) {
+	new google.maps.Marker({
+		position: new google.maps.LatLng(48.323039, 25.917981),
+		map: map,
+		title: a,
+		icon: 'img/icons/marker.png'
+	}).addListener("click", function() {
+		window.open('https://www.google.com/maps/dir/?api=1&map_action=map,Ukraine,Chernivtsi&destination=Zavodska Street,37','_blank');
+	})
+}
